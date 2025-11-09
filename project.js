99a49ebf959585083d9fccabc95cb1e9e5b89eaf
nikhilpahan <nikhilpahan02@gmail.com>
2025-11-09 14:01:50 +0530

    Add Amount Fuction Done

--- project.js
+++ project.js
@@ -9,7 +9,30 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-  const depositAmount = prompt("Enter a deposit amount: ")
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    console.log("Invalid deposit amount, please try again.");
+    } else {
+    return numberDepositAmount;
+    }
+  }
+
 }
 
-deposit();
+const getNumberOfLines = () => {
+      while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+        console.log("Invalid number of lines, please try again.");
+    } else {
+    return numberOfLines;
+    }
+  }
+}
+
+const depositAmount = deposit();
