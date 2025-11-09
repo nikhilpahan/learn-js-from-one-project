ae8d9bba467a700bc6589f4346af793d040e7225
nikhilpahan <nikhilpahan02@gmail.com>
2025-11-09 15:16:16 +0530

    Added bet functionality

--- project.js
+++ project.js
@@ -35,4 +35,21 @@ const getNumberOfLines = () => {
   }
 }
 
-const depositAmount = deposit();
+const getBet = (balance, lines) => {
+      while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+        console.log("Invalid bet, please try again.");
+    } else {
+    return numberBet;
+    }
+  }
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
+
